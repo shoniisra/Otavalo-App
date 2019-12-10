314078d8ea87731ebfb8d77ffd1fce4318fcc275
Shoniisra <jivillacis4@espe.edu.ec>
2019-12-10 13:33:51 -0500

    antes de anadir video player

--- src/app/pages/narrativo/narrativo.page.ts
+++ src/app/pages/narrativo/narrativo.page.ts
@@ -26,7 +26,7 @@ export class NarrativoPage implements OnInit {
       pagina: "/leyenda2"
     },
     {
-      titulo: "MAMA PUMA",
+      titulo: "NINA PACCHA",
       subtitulo:"Leyenda",
       historia:"Cuenta la leyenda que la sequía azotaba a toda la región y, por tanto, había que sacrificar una doncella para calmar las iras del «Taita» Imbabura. Una hermosa indígena llamada Nina Paccha fue la elegida, pero su joven enamorado, Guatalquí, no estaba dispuesto a perderla, por lo que huyeron juntos. El pueblo los siguió y cuando iban a ser alcanzados,el cielo se iluminó y Nina Paccha desapareció. Y mientras el pueblo no salía de su asombro, una fuerte lluvia empezó a caer sobre los campos",
       imagen: "../../../assets/img/ley1.png",
